db94c8bc389c19e6a7eb74544443352a0abfb6b1
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2019-07-12 14:22:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Discord.Client();
 const adapter = new FileSync('database.json');
 const db = low(adapter);
 
-var changed = 0
+
 var number_random = 0;
 var prefix = ("CC!")
 var party_launch = false;
@@ -165,13 +165,10 @@ if(message.content.startsWith(prefix + "Say")){//say 1
         message.channel.send({embed});
          
     }
-}
-
-    if (changed == 2){//say 2
-
-     
+    if (changed == 2){
         let args = message.content.split(" ").slice(1)
         let thingToEcho = args.join(" ")
+        
         const embed = new Discord.RichEmbed()
         .setTitle("឵឵ ឵឵")
         .setColor(0x0086AE)
@@ -183,5 +180,8 @@ if(message.content.startsWith(prefix + "Say")){//say 1
         message.channel.send({embed});
          
     }
+}else{
+    message.channel.send("error")
+}
 }
 )
